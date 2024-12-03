d559e126aa7dd4f6a1b6a9bab1dbe00e4837a343
Robert Knight <robert@microbit.org>
2024-12-03 17:07:41 +0000

    More blocks and more strings

--- custom.ts
+++ custom.ts
@@ -3,17 +3,29 @@
  */
 //% weight=100 color=#0fbc11 icon=""
 namespace custom {
-    /**
-     * Returns square of a number
-     * @param value any number
-     */
-    //% blockId=square
-    //% block="square"
-    //% weight=50
-    //% parts="v2"
-    //% group="micro:bit (V2)"
-    //% help=github:pxt-rdk-translation/docs/square
-    export function square(value: number): number {
-        return value * value;
-    }
+  /**
+   * Returns square of a number
+   * @param value any number
+   */
+  //% blockId=square
+  //% block
+  //% weight=50
+  //% parts="v2"
+  //% group="micro:bit (V2)"
+  //% help=github:pxt-rdk-translation/docs/square
+  export function square(value: number): number {
+    return value * value;
+  }
+
+  /**
+   * Returns a random number
+   */
+  //% blockId=randomNumber
+  //% block
+  //% weight=50
+  //% parts="v2"
+  //% group="micro:bit (V2)"
+  export function randomNumber(): number {
+    return 42;
+  }
 }
